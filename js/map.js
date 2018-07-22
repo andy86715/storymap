6eccb37e0534488095e4d318d669cce157e7c72c
andy86715 <andy86715@gmail.com>
2018-07-22 12:19:10 +0800

    三個頁面初版

---
+++ js/map.js
@@ -0,0 +1,43 @@
+// OSM map
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([37.41, 8.82]),
+    zoom: 4
+  })
+});
+
+// show image
+var fileUpload = document.getElementById('fileUpload');
+var canvas  = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+
+function readImage() {
+    if ( this.files && this.files[0] ) {
+        var FR= new FileReader();
+        FR.onload = function(e) {
+           var img = new Image();
+           img.src = e.target.result;
+           img.onload = function() {
+             ctx.drawImage(img, 0, 0, 512, 512);
+           };
+        };       
+        FR.readAsDataURL( this.files[0] );
+    }
+}
+
+fileUpload.onchange = readImage;
+
+canvas.onclick = function(e) {
+  var x = e.offsetX;
+  var y = e.offsetY;
+  ctx.beginPath();
+  ctx.fillStyle = 'black';
+  ctx.arc(x, y, 5, 0, Math.PI * 2);
+  ctx.fill();
+};
