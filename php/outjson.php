f11ef77a813b1bc72c14509c72c1b25075eaac34
andy86715 <andy86715@gmail.com>
2018-08-25 11:14:49 +0800

    圖片上傳&json匯出網址orURL

--- php/outjson.php
+++ php/outjson.php
@@ -1,4 +1,5 @@
 <?php 
+include('imgur.php');
 
 header("Pragma: public");
 header("Expires: 0");
@@ -9,13 +10,22 @@ header("Content-Type: application/download");;
 header("Content-Disposition: attachment;filename=result.json");
 header("Content-Transfer-Encoding: binary ");
 
+// 去除網址反斜線
+if(!empty($_POST['url'])){
+	$URL = stripslashes($_POST['url']);
+}elseif (!empty($url)) {
+	$URL = stripslashes($url);
+}
+// $URL = stripslashes($_POST['url']);
+// echo $URL;
+
 $array = array();
 $student['storymap'] = array();
 $student['storymap']['slide']['date'] = "";
 $student['storymap']['slide']['location'] = array('icon'=>"", 'lat'=>"",'line'=>"",'lon'=>"",'zoom'=>"");
-$student['storymap']['slide']['media'] = array('caption'=>$_POST['Caption'], 'credit'=>$_POST['Credit'], 'url'=>$_POST['url']);
+$student['storymap']['slide']['media'] = array('caption'=>$_POST['Caption'], 'credit'=>$_POST['Credit'], 'url'=>$URL);
 $student['storymap']['slide']['text'] = array('headline'=>$_POST['title'], 'text'=>$_POST['message']); 
 
-echo json_encode($student);
+echo json_encode($student, JSON_UNESCAPED_SLASHES);
 
  ?>
